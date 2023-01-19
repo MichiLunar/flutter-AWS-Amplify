ac9d7142c9ac848cd9aef5e121da1a2ed23f88bb
MichiLunar <michidevelop@gmail.com>
2023-01-19 12:48:13 -0600

    Creacion de Flujo de autenticacion: parte 1

---
+++ android/app/src/main/kotlin/com/example/flutter_services_web_aws_amlify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_services_web_aws_amlify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/pages.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_services_web_aws_amlify/src/pages/login_page.dart';
